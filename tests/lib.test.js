d041f7074920e4c53b6c4628a61f7d23c943a11b
rtorres7 <shortysupreme@gmail.com>
2019-08-03 21:54:13 -0400

    section12_module125

--- tests/lib.test.js
+++ tests/lib.test.js
@@ -1,4 +1,32 @@
-require('../se')
+const lib = require('../section12/lib');
 
-test('absolute - should return a positive number if input is positive', () => {
+describe('absolute', () => {
+    it('should return a positive number if input is positive', () => {
+        const result = lib.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a positive number if input is negative', () => {
+        const result = lib.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a zero number if input is zero', () => {
+        const result = lib.absolute(0);
+        expect(result).toBe(0);
+    });
 });
+
+describe('greet', () => {
+    it('should return the greeting message', () => {
+        const result = lib.greet('Scarlett');
+        expect(result).toMatch(/Scarlett/);
+        expect(result).toContain('Scarlett');
+    });
+});
+
+describe('getCurrencies', () {
+    it('should return supported currencies', () => {
+        const result = lib.getCurrencies(); 
+    }
+})
